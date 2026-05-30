d172b40371ebca0c75ad6802f57198d909994cf6
AlunoLucasZ <lucadoor40@gmail.com>
2026-05-29 21:47:50 -0300

    Initial Commit

---
+++ feature_ativos.py
@@ -0,0 +1,12 @@
+from App import TipoAtivo
+
+dados = {
+    "notebook": None,
+    "tipo_ativo": TipoAtivo
+}
+
+dados = {
+    "hostname": input("Hostname: "),
+    "responsavel": input("Responsável: "),
+    "setor": input("Setor: "),
+}
